2b4e8262f44e2c327873cd10897aa678bcadc857
Haidara <haidaraibrahim963938@gmail.com>
2024-08-11 14:45:55 +0300

    bug fixes and improvements

--- common/constants.py
+++ common/constants.py
@@ -2,10 +2,10 @@ BACK_TO_HOME_PAGE_TEXT = "العودة إلى القائمة الرئيسية�
 BACK_TEXT = "الرجوع 🔙"
 HOME_PAGE_TEXT = "القائمة الرئيسية 🔝"
 
-SOLANA_ADDRESS_PATTERN = r"^[1-9A-HJ-NP-Za-km-z]{32,44}$"
+SOLANA_ADDRESS_PATTERN = r"[1-9A-HJ-NP-Za-km-z]{32,44}"
 SOLANA_SA_ADDRESS_PATTERN = rf"(?<!https://dexscreener\.com/solana/){SOLANA_ADDRESS_PATTERN}"
 SOLANA_LINK_PATTERN = r"https://dexscreener\.com/solana/[0-9A-Za-z]{32,44}"
 
-ETH_ADDRESS_PATTERN = r"^0x[0-9a-fA-F]{40}$"
+ETH_ADDRESS_PATTERN = r"0x[0-9a-fA-F]{40}"
 ETH_SA_ADDRESS_PATTERN = rf"(?<!https://dexscreener\.com/ethereum/){ETH_ADDRESS_PATTERN}"
 ETH_LINK_PATTERN = r"https://dexscreener\.com/ethereum/0x[0-9a-fA-F]{40}"
--- copy_messages.py
+++ copy_messages.py
@@ -7,38 +7,43 @@ import re
 
 
 def extract_matches(net, text):
-    matches = []
+    matches = set()
     if net in ["solana", "both"]:
+        if not any(t in text for t in ["SOL", "solana"]):
+            return
         pattern = re.compile(SOLANA_LINK_PATTERN)
         solana_match = re.findall(pattern, text)
         if not solana_match:
             pattern = re.compile(SOLANA_SA_ADDRESS_PATTERN)
             solana_match = re.findall(pattern, text)
         if solana_match:
-            matches.append(solana_match[0])
+            matches.add(solana_match[0])
     if net in ["eth", "both"]:
+        if not any(t in text for t in ["ETH", "ethereum"]):
+            return
         pattern = re.compile(ETH_LINK_PATTERN)
         eth_match = re.findall(pattern, text)
         if not eth_match:
             pattern = re.compile(ETH_SA_ADDRESS_PATTERN)
             eth_match = re.findall(pattern, text)
         if eth_match:
-            matches.append(eth_match[0])
+            matches.add(eth_match[0])
     return matches
 
 
 async def copy_messages(event: events.NewMessage.Event | events.Album.Event):
+    if event.forward:
+        return
     FROM = models.Channel.get_all(for_on=True)
     if event.chat_id not in [ch.id for ch in FROM]:
         return
 
     message: Message = event.message
-
     ch = models.Channel.get_one(ch_id=event.chat_id)
     if message.is_reply and not ch.for_rep:
         return
 
-    matches = extract_matches(net=ch.net, text=message.text)
+    matches = extract_matches(net=ch.net, text=message.message)
     if not matches:
         return
 
