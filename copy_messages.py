5fad2958758e40a2a28c0ec3ffaa5e63dbeae66a
= <=>
2024-12-01 10:26:47 +0000

    handle edited messages, change client to reduce latency

--- copy_messages.py
+++ copy_messages.py
@@ -6,39 +6,67 @@ import models
 import re
 
 
+def find_matches(link_pattern, sa_address_pattern, text):
+    pattern = re.compile(link_pattern)
+    match = re.findall(pattern, text)
+    if not match:
+        pattern = re.compile(sa_address_pattern)
+        match = re.findall(pattern, text)
+    return match
+
+
 def extract_matches(net, text):
     matches = set()
-    if net in ["solana", "both"]:
-        if not any(t in text for t in ["SOL", "solana"]):
-            return
-        pattern = re.compile(SOLANA_LINK_PATTERN)
-        solana_match = re.findall(pattern, text)
-        if not solana_match:
-            pattern = re.compile(SOLANA_SA_ADDRESS_PATTERN)
-            solana_match = re.findall(pattern, text)
-        if solana_match:
-            matches.add(solana_match[0])
     if net in ["eth", "both"]:
-        if not any(t in text for t in ["ETH", "ethereum"]):
-            return
-        pattern = re.compile(ETH_LINK_PATTERN)
-        eth_match = re.findall(pattern, text)
-        if not eth_match:
-            pattern = re.compile(ETH_SA_ADDRESS_PATTERN)
-            eth_match = re.findall(pattern, text)
+        eth_match = find_matches(
+            ETH_LINK_PATTERN,
+            ETH_SA_ADDRESS_PATTERN,
+            text,
+        )
         if eth_match:
             matches.add(eth_match[0])
+    if net in ["solana", "both"]:
+        if net == "both" and eth_match:
+            return
+        eth_match = find_matches(
+            ETH_LINK_PATTERN,
+            ETH_SA_ADDRESS_PATTERN,
+            text,
+        )
+        if eth_match:
+            return
+        solana_match = find_matches(
+            SOLANA_LINK_PATTERN,
+            SOLANA_SA_ADDRESS_PATTERN,
+            text,
+        )
+        if solana_match:
+            matches.add(solana_match[0])
     return matches
 
 
-async def copy_messages(event: events.NewMessage.Event | events.Album.Event):
+async def copy_messages(
+    event: events.NewMessage.Event | events.Album.Event | events.MessageEdited.Event,
+):
     if event.forward:
         return
+
     FROM = models.Channel.get_all(for_on=True)
     if event.chat_id not in [ch.id for ch in FROM]:
         return
 
+    active_bot = models.Bot.get_one(active=True)
+    if not active_bot:
+        return
+    
     message: Message = event.message
+    if models.Message.get_one(
+        from_message_id=message.id,
+        from_channel_id=event.chat_id,
+        to_channel_id=active_bot.id,
+    ):
+        return
+
     ch = models.Channel.get_one(ch_id=event.chat_id)
     if message.is_reply and not ch.for_rep:
         return
@@ -47,9 +75,6 @@ async def copy_messages(event: events.NewMessage.Event | events.Album.Event):
     if not matches:
         return
 
-    active_bot = models.Bot.get_one(active=True)
-    if not active_bot:
-        return
 
     for m in matches:
         msg = await ClientSingleton().send_message(
